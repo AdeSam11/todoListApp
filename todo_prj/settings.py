01d6f5c360341332bbe5a9a09abffdb8a84c3dd2
AdeSam11 <adetoyinbosam99@gmail.com>
2024-10-05 22:28:27 +0100

    Modify settings'

--- todo_prj/settings.py
+++ todo_prj/settings.py
@@ -38,10 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'todo_app'
+    'todo_app',
+    'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -128,3 +131,6 @@ LOGIN_URL = 'login-page'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIR = [os.path.join(BASE_DIR, 'static')]
+
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWS_CREDENTIALS = True
